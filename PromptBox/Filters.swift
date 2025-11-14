7fdc96ea88aecb1d3e05d610b168d27a10c63145
Jean Sarda <jean.sarda.partner@decathlon.com>
2025-11-14 14:43:35 +0100

    dev: updated @Guides

--- PromptBox/Filters.swift
+++ PromptBox/Filters.swift
@@ -21,18 +21,13 @@ struct Filters {
     }
     
     @Generable
-    enum UserTags: String {
+    enum Requirement: String {
         case bivouackingTolerated
-        case closed
         case crowded
-        case dangerousTrack
         case dogFriendly
         case easyParking
         case familyFriendly
         case limitedParking
-        case notCrowded
-        case poorCondition
-        case privateProperty
         case shaded
         case strollerAccessible
         case technicalSections
@@ -45,18 +40,18 @@ struct Filters {
     @Guide(description: "The allowed difficulties of the route, or none to include all routes.", .count(0...3))
     let difficulties: [Difficulty]
     
-    @Guide(description: "Only true if the user's query mentions terms like 'popular' or 'well rated'")
+    @Guide(description: "Only true if the query mentions terms like 'popular' or 'well rated'")
     let popular: Bool
     
-    @Guide(description: "Whether que user's query mentions the hike/balad must be a loop.")
+    @Guide(description: "Whether the query mentions the hike/balad must be a loop.")
     let specifiesLooping: Bool
     
-    @Guide(description: "Whether que user's query mentions a place or a location.")
+    @Guide(description: "Whether the query mentions a place or a location.")
     let specifiesLocation: Bool
     
     @Guide(description: "If specifiesLocation is true, must contain the location specified by the query. Otherwise must be '**User Position**'")
     let location: String
     
-    @Guide(description: "Explicit requirements the route must have. Can be empty.")
-    let requirements: [UserTags]
+    @Guide(description: "Explicit requirements the route must have. Must be explictly stated in the query. Can be empty.", .minimumCount(0))
+    let requirements: [Requirement]
 }
